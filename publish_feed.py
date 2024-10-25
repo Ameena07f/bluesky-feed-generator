f6250b5ebee06b42de947a25aa9a479a9d185685
Ameena07f <ameena07f@gmail.com>
2024-10-24 22:16:14 -0400

    modified:   publish_feed.py

--- publish_feed.py
+++ publish_feed.py
@@ -14,7 +14,7 @@ PASSWORD: str = 'Zedekian#07f'
 
 # The hostname of the server where feed server will be hosted
 # Ex: feed.bsky.dev
-HOSTNAME: str = 'lovebrush_chronicles.bsky.dev'
+HOSTNAME: str = 'zedie.bsky.dev'
 
 # A short name for the record that will show in urls
 # Lowercase with no spaces.
